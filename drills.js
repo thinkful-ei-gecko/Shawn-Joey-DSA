ef8d200854ae3825323e499166c088ae861867b5
Joey Romo <joeyjr95@gmail.com>
2019-11-14 12:45:56 -0500

    1/2

---
+++ drills.js
@@ -0,0 +1,6 @@
+//1.1      1 21 26 45 28 29  2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+//1.2   every number will be in its own array
+//1.3   21 & 1
+//1.4   [1, 21, 26, 45] AND [2, 9, 28, 29]
+
+//2.1 the pivot is 14 but not 17
